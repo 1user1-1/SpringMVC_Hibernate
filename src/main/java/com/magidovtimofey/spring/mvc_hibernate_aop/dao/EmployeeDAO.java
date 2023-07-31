fc1691fe776ad590a2bfb0bc0966e6f5c928d835
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-07-31 12:08:45 +0300

    Showing list of employees

---
+++ src/main/java/com/magidovtimofey/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package com.magidovtimofey.spring.mvc_hibernate_aop.dao;
+
+import com.magidovtimofey.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
